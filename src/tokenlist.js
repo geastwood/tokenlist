4ff97669320a9e534957072c3a3a03471422f9be
Fei Liu <liu.feiwood@gmail.com>
2014-04-18 22:33:35 +0200

    init commit

---
+++ src/tokenlist.js
@@ -0,0 +1,9 @@
+var tokenlist = (function() {
+
+    return {
+        add: function(target, str) {},
+        remove: function(target, str) {},
+        toggle: function(target, str) {},
+        contains: function(target, str) {}
+    };
+}());
