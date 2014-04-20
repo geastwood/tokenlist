b964286c941440d9cfbd33312d7ec66ff6c14256
Fei Liu <liu.feiwood@gmail.com>
2014-04-20 23:09:01 +0200

    basic functionalities

--- src/tokenlist.js
+++ src/tokenlist.js
@@ -1,33 +1,70 @@
+// TODO: duplicate loop
 var tokenlist = (function() {
+
+    var service = {
+        exsits: function(target, str) {
+            var i, len;
+            for (i = 0, len = target.length; i < len; i++) {
+                if (target[i] === str) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
 
     var factory = function(target, str, type) {
 
         // trim the target string
         target = target.replace(/^\s+|\s+$/g, '');
 
+        targetArr = target.split(/\s+/);
+
         var map = {
             add: function(target, str) {
-                return target + ' ' + str;
+                target.push(str);
+                // TODO: refactor
+                return target.join(' ').replace(/^\s+|\s+$/g, '');
             },
             remove: function(target, str) {
 
+                var i, len, rst = [];
+                for (i = 0, len = target.length; i < len; i++) {
+                    if (target[i] !== str) {
+                        rst.push(target[i]);
+                    }
+                }
+                // TODO: refactor
+                return rst.join(' ').replace(/^\s+|\s+$/g, '');
             },
-            toggle: function(target, str) {
-
+            exsits: function(target, str) {
+                var i, len;
+                for (i = 0, len = target.length; i < len; i++) {
+                    if (target[i] === str) {
+                        return true;
+                    }
+                }
+                return false;
             }
         };
 
-        return map[type](target, str);
+        return map[type](targetArr, str);
     };
     return {
         add: function(target, str) {
             return factory(target, str, 'add');
         },
-        remove: function(target, str) {},
-        toggle: function(target, str) {},
+        remove: function(target, str) {
+            return factory(target, str, 'remove');
+        },
+        toggle: function(target, str) {
+            return factory(target, str, this.contains(target, str) ? 'remove' : 'add');
+        },
 
         // return boolean
-        contains: function(target, str) {},
+        contains: function(target, str) {
+            return factory(target, str, 'exsits');
+        },
 
         item: function(target, index) {}
     };
--- tests/tokenlist.js
+++ tests/tokenlist.js
@@ -16,11 +16,38 @@ describe('api', function() {
     });
 });
 
-describe('add should work', function() {
+describe('add', function() {
     var orig = ' foo ';
-
 
     it('should add correctly', function() {
         expect(tokenlist.add(orig, 'bar')).toBe('foo bar');
+    });
+});
+describe('remove', function() {
+    var orig = ' foo ';
+    orig = tokenlist.add(orig, 'bar');
+    orig = tokenlist.add(orig, 'fb');
+    it('should remove correctly', function() {
+        expect(orig).toBe('foo bar fb');
+        expect(tokenlist.remove(orig, 'fb')).toBe('foo bar');
+    });
+});
+describe('contains', function() {
+    var orig = 'foo bar fb foobarfb';
+    it('should work', function() {
+        expect(tokenlist.contains(orig, 'foo')).toBe(true);
+        expect(tokenlist.contains(orig, 'foo1')).toBe(false);
+        expect(tokenlist.contains(orig, 'foobar')).toBe(false);
+        expect(tokenlist.contains(orig, 'foobarfb')).toBe(true);
+        expect(tokenlist.contains(orig, 'fb')).toBe(true);
+    });
+});
+describe('toggle', function() {
+    it('should work', function() {
+        expect(tokenlist.toggle('foo bar fb foobarfb', 'foo')).toBe('bar fb foobarfb');
+        expect(tokenlist.toggle('foo bar fb foobarfb', 'fei')).toBe('foo bar fb foobarfb fei');
+        expect(tokenlist.toggle('foo bar fb foobarfb fei', 'fei')).toBe('foo bar fb foobarfb');
+        expect(tokenlist.toggle('', 'foo')).toBe('foo');
+        expect(tokenlist.toggle('foo', 'foo')).toBe('');
     });
 });
